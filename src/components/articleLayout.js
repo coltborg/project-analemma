f231b15031802110568f8296d491e0b86dc16329
coltborg <coltborg@gmail.com>
2019-05-09 08:17:55 -0500

    Add react helmet to articles

--- src/components/articleLayout.js
+++ src/components/articleLayout.js
@@ -4,28 +4,40 @@ import { graphql } from 'gatsby'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
 
 import Layout from './layout'
+import SEO from './seo'
 
-const articleLayout = ({ data: { mdx } }) => (
-  <Layout frontmatter={mdx.frontmatter}>
-    <header style={{ border: '3px dotted purple' }}>
-      <h1>{mdx.frontmatter.title}</h1>
-      <h2>{mdx.frontmatter.date}</h2>
-      <h3>Categories</h3>
-      <ul>
-        {mdx.frontmatter.categories.map(category => (
-          <li key={category}>{category}</li>
-        ))}
-      </ul>
-      <h3>Keywords</h3>
-      <ul>
-        {mdx.frontmatter.keywords.map(keyword => (
-          <li key={keyword}>{keyword}</li>
-        ))}
-      </ul>
-    </header>
-    <MDXRenderer>{mdx.code.body}</MDXRenderer>
-  </Layout>
-)
+const articleLayout = ({ data: { mdx } }) => {
+  const { frontmatter } = mdx
+  const {
+    code: { body },
+  } = mdx
+  return (
+    <Layout>
+      <SEO
+        title={frontmatter.title}
+        keywords={frontmatter.keywords}
+        description={frontmatter.description}
+      />
+      <header style={{ border: '3px dotted purple' }}>
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <h3>Categories</h3>
+        <ul>
+          {frontmatter.categories.map(category => (
+            <li key={category}>{category}</li>
+          ))}
+        </ul>
+        <h3>Keywords</h3>
+        <ul>
+          {frontmatter.keywords.map(keyword => (
+            <li key={keyword}>{keyword}</li>
+          ))}
+        </ul>
+      </header>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Layout>
+  )
+}
 
 articleLayout.propTypes = {
   data: PropTypes.object,
@@ -37,11 +49,12 @@ export const pageQuery = graphql`
   query($id: String!) {
     mdx(id: { eq: $id }) {
       frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        slug
         categories
+        date(formatString: "MMMM DD, YYYY")
+        description
         keywords
+        slug
+        title
       }
       code {
         body
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Proptypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = ({ data }) => {
@@ -20,12 +19,6 @@ const IndexPage = ({ data }) => {
           </li>
         ))}
       </ul>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
     </Layout>
   )
 }
