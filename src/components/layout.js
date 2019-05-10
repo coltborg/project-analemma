6c4769e367053bd18a17cf2c6b52de411c8695c9
coltborg <coltborg@gmail.com>
2019-05-10 07:43:11 -0500

    Add safe new tab links for footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,25 +27,41 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          {/* TODO: Convert to standalone Footer component */}
-          <footer>
-            {/* TODO: Add norel to anchors */}
-            <a href="https://twitter.com/coltborg">Twitter</a> /{' '}
-            <a href="https://www.instagram.com/coltborg/">Instagram</a> /{' '}
-            <a href="https://github.com/coltborg">Github</a> /{' '}
-            <a href="https://codepen.io/coltborg/">CodePen</a>
-            <div>© {new Date().getFullYear()}</div>
-          </footer>
-        </div>
+        <main>{children}</main>
+        <footer>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/coltborg"
+          >
+            Twitter
+          </a>{' '}
+          /{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/coltborg/"
+          >
+            Instagram
+          </a>{' '}
+          /{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/coltborg"
+          >
+            Github
+          </a>{' '}
+          /{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://codepen.io/coltborg/"
+          >
+            CodePen
+          </a>
+          <div>© {new Date().getFullYear()}</div>
+        </footer>
       </>
     )}
   />
