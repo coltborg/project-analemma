82112d67f19e2293e107643d6ae6cc3329d64b77
coltborg <coltborg@gmail.com>
2019-05-26 18:20:44 -0500

    Add emoji to articel layout

--- src/components/articleLayout.js
+++ src/components/articleLayout.js
@@ -7,39 +7,72 @@ import Layout from './layout'
 import SEO from './seo'
 import { H1 } from './designSystem'
 
-const articleLayout = ({ data: { mdx } }) => {
-  const { frontmatter } = mdx
-  const {
-    code: { body },
-  } = mdx
-  return (
-    <Layout>
-      <SEO
-        title={frontmatter.title}
-        keywords={frontmatter.keywords}
-        description={frontmatter.description}
-      />
-      <header className="grid">
-        <H1>{frontmatter.title}</H1>
-        <span className="text-sm">{frontmatter.date}</span>
-        {/* <span>Categories</span>
+const articleLayout = ({
+  data: {
+    mdx: {
+      code: { body },
+    },
+  },
+  // data: {
+  //   mdx: {
+  //     frontmatter: { categories },
+  //   },
+  // },
+  data: {
+    mdx: {
+      frontmatter: { date },
+    },
+  },
+  data: {
+    mdx: {
+      frontmatter: { description },
+    },
+  },
+  data: {
+    mdx: {
+      frontmatter: { emoji },
+    },
+  },
+  data: {
+    mdx: {
+      frontmatter: { keywords },
+    },
+  },
+  data: {
+    mdx: {
+      frontmatter: { title },
+    },
+  },
+}) => (
+  <Layout>
+    <SEO title={title} keywords={keywords} description={description} />
+    <header className="grid">
+      <div className="flex items-baseline">
+        <span
+          aria-hidden="true"
+          className="mr-2 sm:text-2xl md:text-3xl lg:text-4xl hidden sm:block"
+        >
+          {emoji}
+        </span>
+        <div>
+          <H1 style={{ marginBottom: '4px' }}>{title}</H1>
+          <span className="text-sm">{date}</span>
+        </div>
+      </div>
+      {/* <span>Categories</span>
         <ul className="mb-2 text-xs flex flex-wrap">
-          {frontmatter.categories.map(category => (
-            <li
-              className="p-1 mr-1 rounded"
-              key={category}
-            >
+          {categories.map(category => (
+            <li className="p-1 mr-1 rounded" key={category}>
               {category}
             </li>
           ))}
         </ul> */}
-      </header>
-      <article className="mb-12 grid">
-        <MDXRenderer>{body}</MDXRenderer>
-      </article>
-    </Layout>
-  )
-}
+    </header>
+    <article className="mb-12 grid">
+      <MDXRenderer>{body}</MDXRenderer>
+    </article>
+  </Layout>
+)
 
 articleLayout.propTypes = {
   data: PropTypes.object,
@@ -54,6 +87,7 @@ export const pageQuery = graphql`
         categories
         date(formatString: "MMMM DD, YYYY")
         description
+        emoji
         keywords
         slug
         title
