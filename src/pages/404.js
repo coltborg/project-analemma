8dd8834c8875ea314196a74e6c26060d9e3907a1
coltborg <coltborg@gmail.com>
2019-05-27 00:45:07 -0500

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { H1, P } from '../components/designSystem'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col items-center justify-center not-found">
+      <H1>
+        <span aria-hidden="true">😵</span> NOT FOUND
+      </H1>
+      <P>
+        Go back <Link to="/">home</Link>
+      </P>
+    </div>
   </Layout>
 )
 
